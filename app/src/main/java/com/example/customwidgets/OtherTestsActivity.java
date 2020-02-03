601cbdba7c607094bc82a07317b50f774194787d
sarsenovic <s.arsenovic@itsinbox.com>
2020-02-03 17:09:49 +0100

    CustomEditText actions updated

---
+++ app/src/main/java/com/example/customwidgets/OtherTestsActivity.java
@@ -0,0 +1,48 @@
+package com.example.customwidgets;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.testcustomwidgetslibrary.CustomEditText;
+
+public class OtherTestsActivity extends AppCompatActivity implements CustomEditText.CustomEditTextListener {
+    private CustomEditText editText;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_others_test);
+
+        editText = findViewById(R.id.edit_text_test);
+        editText.setCallbackOnEditorActionListener(this);
+        editText.clearFocusAndCursor(EditorInfo.IME_ACTION_SEND, InputType.TYPE_CLASS_TEXT, false);
+        editText.allowContentScrolling();
+    }
+
+//    @Override
+//    public void onEditorActionListener(TextView textView, int actionId, KeyEvent event) {
+//        switch (actionId) {
+//            case EditorInfo.IME_ACTION_SEND:
+//                Toast.makeText(this, "GOOD", Toast.LENGTH_SHORT).show();
+//                break;
+//        }
+//    }
+
+    @Override
+    public void onEditorActionListener(TextView textView, int actionId, KeyEvent event, EditText editText) {
+        switch (actionId) {
+            case EditorInfo.IME_ACTION_SEND:
+//                editText.setCursorVisible(false);
+//                editText.clearFocus();
+                Toast.makeText(this, "GOOD", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
--- customwidgetslibrary/src/main/java/com/example/testcustomwidgetslibrary/CustomEditText.java
+++ customwidgetslibrary/src/main/java/com/example/testcustomwidgetslibrary/CustomEditText.java
@@ -3,10 +3,19 @@ package com.example.testcustomwidgetslibrary;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.TextView;
 
 @SuppressLint("AppCompatCustomView")
 public class CustomEditText extends EditText {
+    private CustomEditTextListener callbackOnEditorActionListener;
+    private boolean canDoSomething = true;
 
     public CustomEditText(Context context) {
         super(context);
@@ -24,5 +33,89 @@ public class CustomEditText extends EditText {
 
     private void setTypography(AttributeSet attributeSet) {
         LoaderForFonts.setTypography(this, attributeSet);
+    }
+
+    public CustomEditTextListener getCallbackOnEditorActionListener() {
+        return callbackOnEditorActionListener;
+    }
+
+    public void setCallbackOnEditorActionListener(CustomEditTextListener callbackOnEditorActionListener) {
+        this.callbackOnEditorActionListener = callbackOnEditorActionListener;
+    }
+
+    //    @Override
+//    protected void onFocusChanged(boolean focused, int direction, Rect previouslyFocusedRect) {
+//        super.onFocusChanged(focused, direction, previouslyFocusedRect);
+//        if (focused)
+//            setCursorVisible(true);
+//        else
+//            setCursorVisible(false);
+//    }
+
+    /**
+     * This function clear focus and cursor on action which is added through function attributes.
+     *
+     * @param editorInfoImeOptions               EditorInfo options (ex. EditorInfo.IME_ACTION_DONE)
+     * @param rawInputType                       Raw input type (ex. InputType.TYPE_CLASS_TEXT)
+     * @param returnTypeOfOnEditorActionListener boolean which will be returned from setOnEditorActionListener
+     */
+    public void clearFocusAndCursor(final int editorInfoImeOptions, int rawInputType, final boolean returnTypeOfOnEditorActionListener) {
+        this.setImeOptions(editorInfoImeOptions);
+        this.setRawInputType(rawInputType);
+
+        this.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                setCursorVisible(false);
+                clearFocus();
+                hideKeyboard();
+                canDoSomething = false;
+                if (callbackOnEditorActionListener != null)
+                    callbackOnEditorActionListener.onEditorActionListener(v, actionId, event, CustomEditText.this);
+
+                return returnTypeOfOnEditorActionListener;
+            }
+        });
+
+        this.setOnClickListener((new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (canDoSomething) {
+                    requestFocus();
+                    setCursorVisible(true);
+                } else {
+                    canDoSomething = true;
+                }
+            }
+        }));
+    }
+
+    private void hideKeyboard() {
+        if (getContext() != null) {
+            InputMethodManager imm = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(this.getWindowToken(), 0);
+        }
+    }
+
+    /**
+     * This function allows editText content scrolling.
+     */
+    public void allowContentScrolling() {
+        this.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                v.getParent().requestDisallowInterceptTouchEvent(true);
+                switch (event.getAction() & MotionEvent.ACTION_MASK) {
+                    case MotionEvent.ACTION_UP:
+                        v.getParent().requestDisallowInterceptTouchEvent(false);
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+
+    public interface CustomEditTextListener {
+        void onEditorActionListener(TextView textView, int actionId, KeyEvent event, EditText editText);
     }
 }
